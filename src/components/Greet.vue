3a3989d452d7c7c2824137e2b6ba1710e76ffd64
haoyu3 <haoyu3@163.com>
2023-03-22 16:38:49 +0800

    init project 0.2.0

---
+++ src/components/Greet.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api/tauri";
+
+const greetMsg = ref("");
+const name = ref("");
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  greetMsg.value = await invoke("greet", { name: name.value });
+}
+</script>
+
+<template>
+  <div class="card">
+    <input id="greet-input" v-model="name" placeholder="Enter a name..." />
+    <button type="button" @click="greet()">Greet</button>
+  </div>
+
+  <p>{{ greetMsg }}</p>
+</template>
--- src/pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import type { AppProps } from "next/app";
-
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
