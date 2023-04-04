1207b0af68e144179e22424df47801dd6355c8e4
haoyu3@163.com <luhaoyu@cmhi.chinamobile.com>
2023-04-04 17:37:52 +0800

    优化chatchat插件

--- src-tauri/plugin/chatchat.js
+++ src-tauri/plugin/chatchat.js
@@ -3,19 +3,31 @@
  *                              这是一个Chat Chat的脚本 V1.0
  * 
  **********************************************************************************/
+const _chatChatMinHeithClassName = "md:min-h-[600px]";
+
 if (window.location.href.indexOf("chat.okis.dev") != -1) {
     try {
-        window.onload = function (e) {
-            process();
-        }
+        // 创建一个MutationObserver实例
+        const observer = new MutationObserver(function (mutations) {
+            // 获取中级空白区域
+            let spaceArea = document.getElementsByClassName(_chatChatMinHeithClassName)[0];
+            if (spaceArea != null) {
+                process();
+            }
+        });
+
+        // 开始观察document，并在节点添加或删除时检测变化
+        observer.observe(document, {
+            childList: true,
+            subtree: true
+        });
+
     } catch (e) {
 
     }
 }
 
-const _chatChatMinHeithClassName = "md:min-h-[600px]";
-
 function process() {
-    let textContentElement = document.getElementsByClassName("md:min-h-[600px]")[0];
+    let textContentElement = document.getElementsByClassName(_chatChatMinHeithClassName)[0];
     textContentElement.classList.remove(_chatChatMinHeithClassName);
 }
